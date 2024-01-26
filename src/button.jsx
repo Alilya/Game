fe6e5879badba7fcd7c560381f0efbd2732f9246
Алина Колесникова <AlinaKolesnikova0101@gmail.com>
2024-01-27 01:52:11 +0300

    ready checked button

--- src/button.jsx
+++ src/button.jsx
@@ -6,8 +6,11 @@ import style from "./styles/button.module.css"
 
 let Button = (props) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [check, setCheck]=useState(false);
+
     let openModal = () => {
         setModalIsOpen(true);
+        setCheck(true)
     };
 
     let closeModal = () => {
@@ -16,23 +19,28 @@ let Button = (props) => {
 
     let content = DB.map((title)=>(
         title.map((str) => (
-            props.number == str.id && props.name==str.name? <ModalContent closeModal={closeModal}
+            props.number == str.id && props.name==str.name? <ModalContent closeModal={closeModal} 
              text={str.text} answer={str.answer}/> : null
         ))
     ))
-   
+      
 
     return (
         <>
-            <button onClick={openModal} className={style.button}>
-                {props.number}
-            </button>
-           
+             {/* <input type="checkbox" id="activate-div" className="activate-div"/>
+            <label for="activate-div">
+                <button onClick={openModal} className={style.button}>
+                    {!check?props.number:0}
+                </button>
+            </label>  */}
+            <button onClick={openModal} className={style.button} id="color">
+                {!check?props.number:0}
+            </button> 
             <Modal isOpen={modalIsOpen}
-                onRequestClose={closeModal} className={style.modal}>    
+                onRequestClose={closeModal} className={style.modal}>
                 {content}
             </Modal>
-            
+
         </>
     )
 
